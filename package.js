bbc6bb16b79fdf75a44fdfa662084514920439bc
Tyson Clugg <tyson@clugg.net>
2015-05-12 14:01:13 +1000

    Fix package.js, version 0.1.0.

--- package.js
+++ package.js
@@ -1,12 +1,13 @@
 Package.describe({
-  summary: "Use plain-text passwords in accounts-password, but warn loudly if not using SSL.",
-  version: '0.1.0'
+  name: 'accounts-secure',
+  version: '0.1.0',
+  summary: 'Use plain-text passwords in accounts-password, but warn loudly if not using SSL.',
+  git: 'https://github.com/tysonclugg/accounts-secure',
+  documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
+  api.versionsFrom('1.0');
   api.use('accounts-password', ['client']);
-  // Export Accounts (etc) to packages using this one.
-  api.imply('accounts-base', ['client']);
-
   api.addFiles('password_client.js', 'client');
 });

46c2f811dcb675e54d2f69fae39ece2b81c2a9ed
Tyson Clugg <tyson@clugg.net>
2015-05-12 14:16:49 +1000

    Correct package name in prep for 0.1.0 release.

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-  name: 'accounts-secure',
+  name: 'tysonclugg:accounts-secure',
   version: '0.1.0',
   summary: 'Use plain-text passwords in accounts-password, but warn loudly if not using SSL.',
   git: 'https://github.com/tysonclugg/accounts-secure',
@@ -9,5 +9,5 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
   api.use('accounts-password', ['client']);
-  api.addFiles('password_client.js', 'client');
+  api.addFiles('password_client.js', ['client']);
 });

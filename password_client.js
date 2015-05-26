a9a1f4dc1047d7df0d7e2bf5869a9c821f3965c3
David Burles <dburles@gmail.com>
2015-05-26 12:01:16 +1000

    override loginWithPassword and use confirm dialog

--- password_client.js
+++ password_client.js
@@ -1,9 +1,14 @@
-Accounts._hashPassword = function (password) {
-  // Complain loudly if not using SSL
-  if (Meteor.connection._stream.socket._base_url.search(/^https:/) != 0) {
-    if(window.prompt("Sending password without encryption, type 'OK' to allow this.", "I'd rather keep my password secure.") !== 'OK') {
-      throw "REFUSING TO SEND AUTHENTICATION CREDENTIALS WITHOUT ENCRYPTION!";
+Meteor.loginWithPassword = (function (original) {
+  return function () {
+    if (Meteor.connection._stream.socket._base_url.search(/^https:/) !== 0) {
+      if(! confirm("Send password without encryption?"))
+        return;
     }
-  }
+
+    return original.apply(this, arguments);
+  };
+}(Meteor.loginWithPassword));
+
+Accounts._hashPassword = function (password) {
   return password;
 };
